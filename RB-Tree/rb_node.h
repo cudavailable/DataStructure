3e2c71633d40e8816679eb34a874f67328875ea3
cudavailable <1598232230@qq.com>
2025-02-06 16:07:10 +0800

    调整文件结构

--- RB-Tree/rb_node.h
+++ RB-Tree/rb_node.h
@@ -7,19 +7,55 @@ enum Color{
 /**
  * Definition of red-black node
  */
+template<typename Value>
 class rb_node{
     friend class rb_tree;
     
 private:
     int key;
+    Value value;
     rb_node* left;
     rb_node* right;
     rb_node* parent;
     Color color = RED; // default: red
 
 public:
-    bool is_left_child(); // whether it's a left child of some node
-    rb_node* uncle(); // return its father's sibling (nullptr for null)
-    rb_node* sibling(); // return its sibling (nullptr for null)
+    /**
+     * whether it's a left child of some node
+     */
+    bool rb_node::is_left_child(){
+        return parent != nullptr && parent->left == this;
+    }
+
+    /**
+     * return its father's sibling (nullptr for null)
+     */
+    rb_node* rb_node::uncle(){
+        if(this->parent == nullptr || this->parent->parent == nullptr){
+            return nullptr;
+        }
+
+        if(this->parent->is_left_child()){
+            return this->parent->parent->right;
+        }else{
+            return this->parent->parent->left;
+        }
+        
+    }
+
+    /**
+     * return its sibling (nullptr for null)
+     */
+    rb_node* rb_node::sibling(){
+        if(this->parent == nullptr){
+            return nullptr;
+        }
+
+        if(this->is_left_child()){
+            return this->parent->right;
+        }else{
+            return this->parent->left;
+        }
+    }
 
 };
--- RB-Tree/rb_tree.h
+++ RB-Tree/rb_tree.h
@@ -1,14 +1,20 @@
 #pragma once
 
 #include "rb_node.h"
-#include "rb_node.cpp"
 
+template<typename Value>
 class rb_tree{
 
 private:
     rb_node* root; // root of red-black tree
+    int count;
 
 public:
-    bool is_red(rb_node* node); // whether the node is red
-    bool is_black(rb_node* node); // whether the node is black 
+    bool rb_tree::is_red(rb_node* cur){
+        return cur!=nullptr && cur->color==Color::RED;
+    }
+
+    bool rb_tree::is_black(rb_node* cur){
+        return cur==nullptr || cur->color==Color::BLACK;
+    }
 };
