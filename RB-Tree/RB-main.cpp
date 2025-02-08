73b1e733ff6cfc6bbdae806d2915754c384ac97b
cudavailable <1598232230@qq.com>
2025-02-08 13:41:32 +0800

    测试fix_double_black函数通过

--- RB-Tree/RB-main.cpp
+++ RB-Tree/RB-main.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
+// get/contains
 void test1(rb_tree<string>* t){
 	t->put(8, "ok");
     t->put(3, "what");
     t->put(2, "are");
     
-    t->display();
-	
-	t->remove(8);
-	t->display(); 
-	
-	t->remove(2);
-	t->display();
-	
-	t->remove(3);
-	t->display();
+    string* s = t->get(3); 
+    if(s == nullptr){
+    	cout << "Null is found!" << endl;
+    }else{
+    	cout << "Content: " << *s << endl;
+    }
+    
+	cout << (t->contains(3)? "Exist" : "Not exist")<< endl;
 }
 
+// delete root node
 void test2(rb_tree<string>* t){
 	t->put(8, "ok");
     t->put(3, "what");
@@ -27,13 +27,20 @@ void test2(rb_tree<string>* t){
     
     t->display();
 	
-	t->remove(8);
-	t->display(); 
-	
+	// delete root with 2 sons
 	t->remove(3);
+	t->display();
+	
+	// delete root with 1 son
+	t->remove(8);
+	t->display();
+	
+	// delete root without son
+	t->remove(2);
 	t->display();
 }
 
+// insert nodes
 void test3(rb_tree<string>* t){
 	t->put(8, "ok");
     t->put(3, "what");
@@ -45,31 +52,65 @@ void test3(rb_tree<string>* t){
     t->put(40, "guy");
     
     t->display();
+}
+
+// 1.delete red node without son
+// 2.test Case4
+// 3.test Case3 
+void test4(rb_tree<string>* t){
+	t->put(8, "ok");
+    t->put(3, "what");
+    t->put(2, "are");
+    t->put(5, "you");
+    t->put(12, "do");
+    t->put(20, "us");
+    t->put(30, "we");
+    t->put(40, "guy");
+
+	t->display();
+    
+    t->remove(40);
+    t->display();
+    
+    // Case4: black sibling with 2 black sons
+    t->remove(30);
+    t->display();
+    
+    t->remove(12);
+    t->display();
+    
+    // Case3: red sibling
+    t->remove(20);
+    t->display();
+}
+
+// delete black node without son
+void test5(rb_tree<string>* t){
+	t->put(8, "ok");
+    t->put(3, "what");
+    t->put(2, "are");
+    t->put(5, "you");
+    t->put(12, "do");
+    t->put(20, "us");
+    t->put(30, "we");
+    t->put(4, "me");
+	t->put(40, "no");
 	
-//	t->remove(8);
-//	t->display(); 
-//	
-//	t->remove(3);
-//	t->display();
+	t->display();
+    
+    // Case5 RR
+//    t->remove(12); 
+//    t->display();
+    
+    // Case5 RL
+//    t->remove(2);
+//    t->display();
 }
 
 int main(){
     rb_tree<string>* t = new rb_tree<string>();
 
-    test3(t);
-	
-    
-//    string* s = t->get(3); 
-//    if(s == nullptr){
-//    	cout << "Null is found!" << endl;
-//    }else{
-//    	cout << "Content: " << *s << endl;
-//    }
-
-//	cout << "Empty: " << t->empty() << endl;
-//	cout << "Size: " << t->size() << endl;
-    
-//    t->display();
+    test4(t);
     
     delete t;
 
